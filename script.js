e88b2fba26a861c8ade6f0e15306b9ef8df72ba6
evbomwan <evbomwanc@gmail.com>
2026-07-08 00:07:45 +0100

    moved the player outiside the gameBoard module

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-// IIFE
-const gameBoard = (() => {
-  const board = ["", "", "", "", "", "", "", "", ""];
-  // object constructor for players
-  function createPlayer(name, marker, position) {
+// player
+ function Player(name, marker) {
     this.name = name;
     this.marker = marker;
   }
-  const player1 = new createPlayer("James", "O");
-  const player2 = new createPlayer("Paul", "X");
+  const player1 = new Player("James", "O");
+  const player2 = new Player("Paul", "X");
   let currentPlayer = player1;
+// IIFE
+const gameBoard = (() => {
+  const board = ["", "", "", "", "", "", "", "", ""];
+//  function for playing a single round
   function playRound(index) {
-    if (board[index]) {
+    if (board[index] !=="") {
       return;
     } else {
       board[index] = currentPlayer.marker;
-      currentPlayer = player2;
+      if (currentPlayer === player1){
+        currentPlayer = player2;
+      } else {
+        currentPlayer = player1;
+      }
+      
     }
   }
   playRound(7);
 })();
+const gameController;
