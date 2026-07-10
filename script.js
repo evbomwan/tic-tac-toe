be01f4623642801adc639c56275b146459ceba1c
evbomwan <evbomwanc@gmail.com>
2026-07-10 17:48:40 +0100

    added the gameOver function for when the game ends

--- script.js
+++ script.js
@@ -6,34 +6,39 @@ function Player(name, marker) {
 const player1 = new Player("James", "O");
 const player2 = new Player("Paul", "X");
 let currentPlayer = player1;
+let gameOver = false;
 // the function that controls the game
 function gameController(index) {
+  if (gameOver) {
+    return;
+  }
   const moveSuccesful = gameBoard.playRound(index);
-  if (!moveSuccesful){
+  if (!moveSuccesful) {
     return;
   }
   const winner = gameBoard.checkWinner();
   if (winner) {
+    gameOver = true;
     console.log(`${winner} wins`);
     return;
   }
-  if (gameBoard.checkDraw()){
-    console.log("It's a draw")
+  if (gameBoard.checkDraw()) {
+    gameOver = true;
+    console.log("It's a draw");
+    return;
   }
   switchPlayer();
 }
- function switchPlayer() {
-    if (currentPlayer === player1) {
-      currentPlayer = player2;
-    } else {
-      currentPlayer = player1;
-    }
+function switchPlayer() {
+  if (currentPlayer === player1) {
+    currentPlayer = player2;
+  } else {
+    currentPlayer = player1;
   }
+}
 // IIFE
 const gameBoard = (() => {
-  const board = ["", "", "",
-                 "", "", "", 
-                 "", "", ""];
+  const board = ["", "", "", "", "", "", "", "", ""];
   //  function for playing a single round
   function playRound(index) {
     if (board[index] !== "") {
@@ -41,27 +46,23 @@ const gameBoard = (() => {
     } else {
       board[index] = currentPlayer.marker;
       return true;
-      
     }
   }
   // function to check the winner
-  function checkWinner(){
+  function checkWinner() {
     const winningCombinations = [
-      [0,1,2],
-      [3,4,5],
-      [6,7,8],
-      [0,3,6],
-      [1,4,7],
-      [2,5,8],
-      [0,4,8],
-      [2,4,6]
-    ]
-    for (const combination of winningCombinations){
-      const [a,b,c] = combination;
-      if (board[a] !== "" &&
-          board[a] === board[b] &&
-          board[b] === board[c]
-      ) {
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (const combination of winningCombinations) {
+      const [a, b, c] = combination;
+      if (board[a] !== "" && board[a] === board[b] && board[b] === board[c]) {
         return board[a];
       }
     }
@@ -69,12 +70,16 @@ const gameBoard = (() => {
   }
   // function for checking for draw
   function checkDraw() {
-    return board.every(square => square !== "");
+    return board.every((square) => square !== "");
+  }
+  // to get the board
+  function getBoard() {
+    return board;
   }
   return {
     playRound,
     checkWinner,
     checkDraw,
+    getBoard,
   };
-  
 })();
